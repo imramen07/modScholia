2619668a8cd076d2cd27a844a56bc6f562b83ad0
imramen07 <chramen07@gmail.com>
2026-06-16 14:31:30 +0530

    feat: use content+ source + page as key

--- utils/deduplication.py
+++ utils/deduplication.py
@@ -1,10 +1,17 @@
 def deduplicate_docs(docs):
-    seen = set()
-    unique_docs = []
+    seencont = set()
+    unique = []
 
     for doc in docs:
-        text = doc.page_content.strip()
-        if text not in seen:
-            seen.add(text)
-            unique_docs.append(doc)
-    return unique_docs
+        # add - use content, source and page as info"
+        info = (doc.page_content, doc.metadata.get("source", ""), doc.metadata.get("page", 0))
+        if info not in seencont:
+            seencont.add(info)
+            unique.append(doc)
+
+        # use if source fails
+        #text = doc.page_content.strip()
+        #if text not in seencont:
+            #seencont.add(text)
+            #unique.append(doc)
+    return unique
