69771139f846fef39bf4f0c691f74c6c95d235d1
imramen07 <chramen07@gmail.com>
2026-06-16 14:18:17 +0530

    feat: add progress bar, spinner

--- ingestion/processor.py
+++ ingestion/processor.py
@@ -1,11 +1,20 @@
-from ingestion.loader import load_pdf
+import streamlit as st
+from ingestion.loader import pdf_loader
 from ingestion.splitter import split_docs
 
 def process_files(file_data):
     all_pages = []
-    for name, data in file_data:
-        pages = load_pdf(name, data)
-        all_pages.extend(pages)
+    # add - loading bar
+    loadingbar = st.progress(0, text = "Loading PDFs...")
 
-    chunks = split_docs(all_pages)
+    # iterate index (i) for loadingbar
+    for i, (name, data) in enumerate(file_data):
+        pages = pdf_loader(name, data)
+        all_pages.extend(pages)
+        loadingbar.progress((i+1)/len(file_data))
+    loadingbar.empty()
+
+    # set spinner for chunk splitting
+    with st.spinner("Splitting into chunks..."):
+        chunks = split_docs(all_pages)
     return all_pages, chunks
