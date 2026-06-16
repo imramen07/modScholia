6afbb2ea4e18c01aea1ed0ee78c5d8f956358796
imramen07 <chramen07@gmail.com>
2026-06-16 14:11:01 +0530

    fix: add cuda oom falllback, test embedding query - add trust remote code = False for safety - Test embedding with dummy query - fallback to cpu when cuda oom

--- core/embeddings.py
+++ core/embeddings.py
@@ -3,18 +3,25 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from config.settings import EMBEDDING_MODEL
 
 @st.cache_resource
-def load_embeddings(device):
-
-    if device not in ["cuda", "cpu"]:
-        device = "cpu"
-
+def init_and_load_embeddings(device):
+    # removed device check from here
     try:
         embeddings = HuggingFaceEmbeddings(
             model_name = EMBEDDING_MODEL,
-            model_kwargs = {"device": device},
+            # for safety, keep trust_remote_code false
+            model_kwargs = {"device": device,
+                            "trust_remote_code": False},
             encode_kwargs = {"normalize_embeddings": True}
         )
+        # test a testing string to spot failures
+        _ = embeddings.embed_query("test me")
         return embeddings
     
     except Exception as e:
+        # here fallback to cpu if cuda out of mem
+        # handles large chunking memory failures and syystem crashes
+        if device == "cuda" and "CUDA out of memory" in str(e):
+            st.warning("CUDA out of memory, Fallback to CPU")
+            return init_and_load_embeddings("cpu")
+        # if cpu fails too
         raise RuntimeError(f"Failed to load embedding model: {e}")
