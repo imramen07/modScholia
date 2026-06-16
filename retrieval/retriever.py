d088e4607ba099f6222dd04412a8b8d39d3c5ccd
imramen07 <chramen07@gmail.com>
2026-06-16 14:29:17 +0530

    refactor: moved better query to query rewriter

--- retrieval/retriever.py
+++ retrieval/retriever.py
@@ -1,20 +1,19 @@
-import re
 from config.settings import TOP_K, FETCH_K
-
-def clean_query(query: str):
-    return re.sub(r"\s+", " ", query).strip().lower()
+from pipeline.query_rewriter import better_query
 
 def retrieve_docs(db, query):
     
-    query = clean_query(query)
+    query = better_query(query)
 
     try:
+        # prioritize mmr
         docs = db.max_marginal_relevance_search(
             query,
             k = TOP_K,
             fetch_k = FETCH_K
         )
 
+        # sim search if mmr fails
         if not docs:
             docs = db.similarity_search(query, k = TOP_K)
         
