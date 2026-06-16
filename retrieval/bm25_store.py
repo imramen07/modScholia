a7ea5ffd0ffc47c45b3ca579150f5a21584b8f8a
imramen07 <chramen07@gmail.com>
2026-06-16 14:25:33 +0530

    feat: use NLTK tokenization and stopwords for bm25 - add thistokenize for preprocessing - del simple split tokenization

--- retrieval/bm25_store.py
+++ retrieval/bm25_store.py
@@ -1,16 +1,34 @@
+# major changes
+# using nltk for better tokenization
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
 from rank_bm25 import BM25Okapi
 from utils.deduplication import deduplicate_docs
+
+# set stopwords using nltk
+swords = set(stopwords.words('english'))
 
 class BM25Store:
     
     def __init__(self, docs):
         self.docs = deduplicate_docs(docs)
-        self.corpus = [doc.page_content.lower().split() for doc in self.docs]
+        self.corpus = [self.thisTokenize(doc.page_content) for doc in self.docs]
         self.bm25 = BM25Okapi(self.corpus)
     
+    # add - tokenizer
+    def thisTokenize(self, text):
+        toks = word_tokenize(text.lower())
+        return[
+            t for t in toks if t.isalnum() and
+            t not in swords
+        ]
+
     def search(self, query, k = 5):
-        tokenized_query = query.lower().split()
-        scores = self.bm25.get_scores(tokenized_query)
+        #tokenized_query = query.lower().split()
+        tokedQuery = self.thisTokenize(query)
+        scores = self.bm25.get_scores(tokedQuery)
 
         ranked = sorted(
             zip(self.docs, scores),
